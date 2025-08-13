dfb388907b525439deae808335edfcf77c962972
void <void@freewifi.club>
2025-08-13 10:10:36 +0200

    Added PEAK blocking input check to OverrideKey

--- NametagModifier/OverrideClock.cs
+++ NametagModifier/OverrideClock.cs
@@ -8,19 +8,22 @@ public static class OverrideClock {
     public static bool Clock() {
         var overrideTime = SettingsHandler.Instance.GetSetting<OverrideTime>().Value;
         
-        // Reset Override Behavior if player presses OverrideKey while already overridden
-        if (Input.GetKeyDown(NametagModifier.OverrideKey.Value) && Time.realtimeSinceStartupAsDouble - LastTimeSetToTrue < overrideTime) {
-            LastTimeSetToTrue = float.MinValue;
-            return false;
-        }
-        // Compare Time.realtimeSinceStartupAsDouble to LastTimeSetToTrue, return true if less than overrideTime
-        if (Time.realtimeSinceStartupAsDouble - LastTimeSetToTrue < overrideTime){
-            return true;
-        }
-        // If override keybind is pressed, set LastTimeSetToTrue to realtimeSinceStartup
-        if (Input.GetKeyDown(NametagModifier.OverrideKey.Value)) {
-            LastTimeSetToTrue = Time.realtimeSinceStartup;
-            return true;
+        // Check if PEAK is currently blocking window input, such as in menus.
+        if (!GUIManager.instance.windowBlockingInput) {
+            // Reset Override Behavior if player presses OverrideKey while already overridden
+            if (Input.GetKeyDown(NametagModifier.OverrideKey.Value) && Time.realtimeSinceStartupAsDouble - LastTimeSetToTrue < overrideTime) {
+                LastTimeSetToTrue = float.MinValue;
+                return false;
+            }
+            // Compare Time.realtimeSinceStartupAsDouble to LastTimeSetToTrue, return true if less than overrideTime
+            if (Time.realtimeSinceStartupAsDouble - LastTimeSetToTrue < overrideTime){
+                return true;
+            }
+            // If override keybind is pressed, set LastTimeSetToTrue to realtimeSinceStartup
+            if (Input.GetKeyDown(NametagModifier.OverrideKey.Value)) {
+                LastTimeSetToTrue = Time.realtimeSinceStartup;
+                return true;
+            }
         }
         return false;
     }
