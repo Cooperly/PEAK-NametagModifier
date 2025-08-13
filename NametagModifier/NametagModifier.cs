453e25c9405d5de324963536bc949deb58aa4774
void <void@freewifi.club>
2025-08-13 09:44:39 +0200

    Cleanup NametagModifier.cs

--- NametagModifier/NametagModifier.cs
+++ NametagModifier/NametagModifier.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 
 namespace NametagModifier;
 
-[BepInPlugin("club.freewifi.void.NameTagModifier", "NametagModifier", "1.0.0")]
+[BepInPlugin("club.freewifi.void.NameTagModifier", "NametagModifier", "1.0.1")]
 public class NametagModifier : BaseUnityPlugin {
     public static ConfigEntry<KeyCode> OverrideKey = null!;
 
@@ -20,7 +20,11 @@ public class NametagModifier : BaseUnityPlugin {
 
     private void Awake() {
         
-        OverrideKey = Config.Bind("Keybinds", "Override Key", KeyCode.T, "Pressing this key will show all nametags of all players for a set amount of time defined in the game settings, regardless of distance or camera angle.");
+        OverrideKey = Config.Bind(
+            "Keybinds",
+            "Override Key",
+            KeyCode.T,
+            "Pressing this key will show all nametags of all players for a set amount of time defined in the game settings, regardless of distance or camera angle.");
         
         SettingsRegistry.Register("NametagModifier");
         var harmony = new Harmony("NametagModifier");
